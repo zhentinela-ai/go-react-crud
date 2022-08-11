218b9067388b9fe552447fd6835cb30f756eea76
Zhent <jdcaballeror@correo.udistrital.edu.co>
2022-08-11 08:26:47 -0500

    create and get done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [name, setName] = useState("");
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await fetch(import.meta.env.VITE_API + "/users");
+      const data = await response.json();
+      setUsers(data.users);
+    }
+    loadUsers();
+  }, [users]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch(import.meta.env.VITE_API + "/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
   return (
     <div>
       <h1>Hello World</h1>
@@ -11,6 +38,22 @@ function App() {
       >
         Obtener datos
       </button>
+      <br />
+
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Coloca tu username"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button>Guardar</button>
+      </form>
+
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>{user.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
--- mian.go
+++ mian.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"github.com/zhentinela-ai/go-react-crud/models"
 )
 
 func main() {
@@ -16,17 +22,67 @@ func main() {
 	}
 
 	app := fiber.New()
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017/gomongodb"))
+
+	if err != nil {
+		panic(err)
+	}
 
 	app.Use(cors.New())
 
 	app.Static("/", "./client/dist")
 
-	app.Get("/users", func(c *fiber.Ctx) error {
+	// app.Get("/users", func(c *fiber.Ctx) error {
+	// 	return c.JSON(&fiber.Map{
+	// 		"data": "usuarios desde el backend",
+	// 	})
+	// })
+
+	app.Post("/users", func(c *fiber.Ctx) error {
+		var user models.User
+
+		c.BodyParser(&user)
+
+		coll := client.Database("gomongodb").Collection("users")
+		databaseName := coll.Database().Name()
+		fmt.Fprintln(os.Stdout, "Connected to MongoDB", databaseName)
+		result, err := coll.InsertOne(context.TODO(), bson.D{{
+			Key:   "name",
+			Value: user.Name,
+		}})
+		if err != nil {
+			panic(err)
+		}
+
 		return c.JSON(&fiber.Map{
-			"data": "usuarios desde el backend",
+			"data": result,
 		})
 	})
 
-	app.Listen(":" + port)
+	app.Get("/users", func(c *fiber.Ctx) error {
+		var users []models.User
+
+		coll := client.Database("gomongodb").Collection("users")
+		databaseName := coll.Database().Name()
+		fmt.Fprintln(os.Stdout, "Connected to MongoDB", databaseName)
+		results, err := coll.Find(context.TODO(), bson.M{})
+
+		if err != nil {
+			panic(err)
+		}
+
+		for results.Next(context.TODO()) {
+			var user models.User
+			results.Decode(&user)
+			users = append(users, user)
+		}
+
+		return c.JSON(&fiber.Map{
+			"users": users,
+		})
+
+	})
+
 	fmt.Println("Server started on port 4000")
+	app.Listen(":" + port)
 }
